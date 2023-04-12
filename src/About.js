fdae215296c3bc6da27b4cd28cb9655c66866d09
Serex <adanjogohe@gmail.com>
2023-04-12 10:23:35 +0100

    feat: Tailwind configs and things

--- src/About.js
+++ src/About.js
@@ -6,14 +6,16 @@ const skills = [
     { name: 'Java', rating: 4 },
     { name: 'Spring', rating: 4 },
     { name: 'C#', rating: 3 },
-    // ...
+    { name: 'Python', rating: 3 },
+    { name: 'TypeScript', rating: 3 },
 ];
 
 const tools = [
     { name: 'JetBrains Suit', rating: 4 },
     { name: 'Jira', rating: 4 },
     { name: 'TeamCity', rating: 3 },
-    // ...
+    { name: 'Jenkins', rating: 4 },
+    { name: 'VSCode', rating: 3 },
 ];
 
 function About() {
@@ -22,13 +24,22 @@ function About() {
             <div className="presentation">
                 <h1>About</h1>
                 <p className="presentation-text">
-                    I am a passionate programmer who loves my work, strives to be responsible, and is eager to learn new things. Throughout my career, I have consistently shown dedication to my projects, always seeking to improve and stay up-to-date with the latest technological advancements. My commitment to excellence has led me to develop expertise in various programming languages and tools, allowing me to create innovative solutions for a wide range of applications.
+                    I am a passionate programmer who loves technology, strives to be responsible, and is eager to learn new things.
+                    Throughout my career, I have consistently shown dedication to my projects,
+                    always seeking to improve and stay up-to-date with the latest technological advancements.
+                    My commitment to excellence has led me to develop expertise in various programming languages and tools,
+                    allowing me to create innovative solutions for a wide range of applications.
                 </p>
                 <p className="presentation-text">
-                    With a strong foundation in problem-solving, communication, and teamwork, I am confident in my ability to tackle any challenge that comes my way. My enthusiasm for learning and sharing knowledge has not only propelled my professional growth but has also allowed me to contribute positively to the teams and communities I am part of.
+                    With a strong foundation in problem-solving, communication, and teamwork,
+                    I am confident in my ability to tackle any challenge that comes my way.
+                    My enthusiasm for learning and sharing knowledge has not only propelled my professional growth
+                    but has also allowed me to contribute positively to the teams and communities I am part of.
                 </p>
                 <p className="presentation-text">
-                    As a developer, I understand the importance of staying adaptable and embracing change. My curiosity and drive for continuous improvement have enabled me to excel in various roles and environments. I am excited to continue exploring new horizons and making a meaningful impact in the world of technology.
+                    As a developer, I understand the importance of staying adaptable and embracing change.
+                    My curiosity and drive for continuous improvement have enabled me to excel in various roles and environments.
+                    I am excited to continue exploring new horizons and making a meaningful impact in the world of technology.
                 </p>
             </div>
             <div className="about-sections">
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './NavBar.css';
 import myPhoto from './personal-photo.jpg';
+import { motion } from 'framer-motion';
 
 function NavBar() {
     const [loaded, setLoaded] = useState(false);
@@ -10,21 +11,23 @@ function NavBar() {
     }, []);
 
     return (
-        <nav className={`nav-bar ${loaded ? 'nav-loaded' : ''}`}>
-            <img src={myPhoto} alt="Your Name" className="photo" />
-            <ul className="nav-links">
+        <nav className={`nav-bar fixed top-0 left-0 h-full w-64 md:w-80 bg-gray-800 p-6 ${loaded ? 'nav-loaded' : ''}`}>
+            <div className="photo-container">
+                <motion.img src={myPhoto} alt="Your Name" className="photo mx-auto mb-8 rounded-full w-32 h-32 object-cover" />
+            </div>
+            <ul className="nav-links flex flex-col gap-4">
                 <li className="nav-link-item">
-                    <a href="#about" className="nav-link">
+                    <a href="#about" className="nav-link text-gray-300 text-lg font-semibold hover:text-white">
                         About
                     </a>
                 </li>
                 <li className="nav-link-item">
-                    <a href="#projects" className="nav-link">
+                    <a href="#projects" className="nav-link text-gray-300 text-lg font-semibold hover:text-white">
                         Projects
                     </a>
                 </li>
                 <li className="nav-link-item">
-                    <a href="#contact" className="nav-link">
+                    <a href="#contact" className="nav-link text-gray-300 text-lg font-semibold hover:text-white">
                         Contact
                     </a>
                 </li>
